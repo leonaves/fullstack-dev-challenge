8fbd36f10cf0001aafdf6375d66e9f7ac152d5fe
Leon Aves <leonaves1@gmail.com>
2018-06-10 16:53:50 +0100

    Refactor compound interest calculation.

--- __tests__/getSavings.test.js
+++ __tests__/getSavings.test.js
@@ -1,12 +1,31 @@
 const getSavings = require('../getSavings');
 
-test('Can calculate compound interest on a principal investment', () => {
-  const savings = getSavings(1000, 0, 0.1, 3);
+test('Can calculate yearly compounding interest on a principal investment', () => {
+  const savings = getSavings(1000, 0, 0.1, 12);
 
-  expect(savings[0].month).toBe(1);
-  expect(savings[0].amount).toBeCloseTo(1007.97, 2);
-  expect(savings[1].month).toBe(2);
-  expect(savings[1].amount).toBeCloseTo(1016.01, 2);
-  expect(savings[2].month).toBe(3);
-  expect(savings[2].amount).toBeCloseTo(1024.11, 2);
+  expect(savings[0]).toBeCloseTo(1000, 2);
+  expect(savings[1]).toBeCloseTo(1000, 2);
+  expect(savings[11]).toBeCloseTo(1000, 2);
+  expect(savings[12]).toBeCloseTo(1100, 2);
+  expect(savings[13]).toBeUndefined();
+});
+
+test('Can calculate monthly compounding interest on a principal investment', () => {
+  const savings = getSavings(1000, 0, 0.025, 12, 12);
+
+  expect(savings[0]).toBeCloseTo(1000, 2);
+  expect(savings[1]).toBeCloseTo(1002.08, 2);
+  expect(savings[11]).toBeCloseTo(1023.16, 2);
+  expect(savings[12]).toBeCloseTo(1025.29, 2);
+});
+
+test('Can calculate quarterly compounding interest on a principal investment', () => {
+  const savings = getSavings(1000, 0, 0.05, 12, 4);
+
+  expect(savings[0]).toBeCloseTo(1000, 2);
+  expect(savings[1]).toBeCloseTo(1000, 2);
+  expect(savings[3]).toBeCloseTo(1012.50, 2);
+  expect(savings[6]).toBeCloseTo(1025.16, 2);
+  expect(savings[11]).toBeCloseTo(1037.97, 2);
+  expect(savings[12]).toBeCloseTo(1050.95, 2);
 });
--- getSavings.js
+++ getSavings.js
@@ -1,12 +1,31 @@
-const getSavings = (initial, monthly, rate, months = 50, compoundedPa = 1) => (
+const getSavingsScalar = (initial, monthly, rate, months = 50, compoundsPa = 1) => (
   [...Array(months)].map((v, index) => {
     const month = index + 1;
 
+    const compoundedValue = initial * (1 + (rate/compoundsPa)) ** (compoundsPa * (1/12) * month);
+    const contributions = (monthly * (12/compoundsPa)) * (((1 + rate/compoundsPa) ** (compoundsPa * ((1/12) * month)) - 1) / (rate/compoundsPa));
+
     return {
       month,
-      amount: initial * (1 + (rate/compoundedPa)) ** (compoundedPa * (1/12) * month),
+      amount: compoundedValue + contributions,
     };
   })
 );
+
+const getSavings = (initial, monthly, rate, months = 50, compoundsPa = 1) => {
+  const values = [initial];
+
+  for (let month = 1; month <= months; month++) {
+    let value = values[month - 1];
+
+    if (month % (12/compoundsPa) === 0) {
+      value += value * (rate / compoundsPa);
+    }
+
+    values.push(value);
+  }
+
+  return values;
+};
 
 module.exports = getSavings;
